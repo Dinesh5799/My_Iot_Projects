201769b556134da991dc514c257de255e7e2cdee
Dinesh Kumar <dineshchunduspecial@gmail.com>
2017-05-24 04:14:55 +0530

    Add files via upload
    Boardo is an IOT device which is used to count the no of users who read
    a particular notice from a notice board. In making of this project I came across few
    difficulties using sensors like ultrasonic which later came to know that was not right
    one and shifted to long range IR which gave required results up to better extents. For
    boardo I used Arduino as the main component and coded in embedded c.

---
+++ irsensor/irsensor.ino
@@ -0,0 +1,28 @@
+int led_pin=7;
+int led = 13;
+int count=0,count1=0;
+int i=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led_pin,INPUT);
+pinMode(led, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, LOW);
+int x=digitalRead(led_pin);
+for(i=0;i<300;i++)
+{
+  if(x==HIGH)
+   count++;
+}
+if(count==300)
+{
+  digitalWrite(led, HIGH);
+  delay(500);
+  Serial.println("Object detected");
+  Serial.println(count1++);
+}
+}
---
+++ ultrasonic/ultrasonic.ino
@@ -0,0 +1,44 @@
+int trigPin = 9;
+int echoPin=10;
+int led=7;
+long duration=0;
+int distance=0;
+int count=0,count1=0,i;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, LOW);
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  int x=digitalRead(echoPin);
+  duration=pulseIn(echoPin,HIGH);
+  Serial.print("Duration now=");
+  Serial.println(duration);
+  distance=duration*0.034/2;
+  for(i=0;i<15000;i++)
+  {
+    if(distance<120) count++;
+  }
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  //Serial.print("Count now=");
+  //Serial.println(count);
+  if(count>15000)
+  {
+    digitalWrite(led, HIGH);
+    delay(500);
+    Serial.println("Object detected");
+    Serial.println(count1++);
+    count=0;
+    delay(3000);
+  }
+}
